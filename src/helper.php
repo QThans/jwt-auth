f049851a2f24aabe4434fc19ad9c5d62434f4677
Thans <360641274@qq.com>
2019-08-19 00:18:58 +0800

    fix command load

--- src/helper.php
+++ src/helper.php
@@ -3,9 +3,16 @@
 use thans\jwt\command\SecretCommand;
 use thans\jwt\provider\JWT as JWTProvider;
 use think\Console;
+use think\facade\App;
 
-Console::addDefaultCommands([
-    SecretCommand::class,
-]);
+if (strpos(App::version(), '6.0') !== false) {
+    Console::addCommands([
+        SecretCommand::class,
+    ]);
+} else {
+    Console::addDefaultCommands([
+        SecretCommand::class,
+    ]);
+}
 
 (new JWTProvider(new \think\Request()))->init();
